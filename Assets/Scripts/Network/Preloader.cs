f0f7824850bc8bd4060f7ceba6422c170634789e
Julia Butenhoff <cockatoo242@gmail.com>
2021-10-06 13:26:56 -0500

    Setup Preloader and Created Scenes
    - Added Preloader Scene
    - Created Lobby Scene As Template
    - Started Basic Design of Title Screen

--- Assets/Scripts/Network/Preloader.cs
+++ Assets/Scripts/Network/Preloader.cs
@@ -1,18 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
+using MLAPI;
+using MLAPI.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Preloader : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Preloader : MonoBehaviour {
+    private NetworkManager netManager;
+
+    void Start() {
+        netManager = GetComponentInParent<NetworkManager>();
+
+        if (Application.isEditor) return;
+
+        var args = GetCommandlineArgs();
+
+        // Check for the dedicated server command flags
+        if (args.TryGetValue("-mlapi", out string mlapiValue)) { 
+            if (mlapiValue == "server") {
+                // Start the server
+                netManager.StartServer();
+
+                // Swap to the lobby scene to await players
+                NetworkSceneManager.SwitchScene("Lobby");
+            }
+        }
+
+        // If not a dedicated server
+        // Swap to Title Screen (as client)
+        SceneManager.LoadScene("TitleScene");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private Dictionary<string, string> GetCommandlineArgs() {
+        Dictionary<string, string> argDictionary = new Dictionary<string, string>();
+
+        var args = System.Environment.GetCommandLineArgs();
+
+        for (int i = 0; i < args.Length; ++i) {
+            var arg = args[i].ToLower();
+            if (arg.StartsWith("-")) {
+                var value = i < args.Length - 1 ? args[i + 1].ToLower() : null;
+                value = (value?.StartsWith("-") ?? false) ? null : value;
+
+                argDictionary.Add(arg, value);
+            }
+        }
+        return argDictionary;
     }
 }
