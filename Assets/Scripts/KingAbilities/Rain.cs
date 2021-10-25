debf981e38216c2022da12fe93285ec40bb092cf
Vincent Wheat <waffleater7@gmail.com>
2021-10-25 14:18:13 -0500

    Rain and Snow

--- Assets/Scripts/KingAbilities/Rain.cs
+++ Assets/Scripts/KingAbilities/Rain.cs
@@ -5,25 +5,40 @@ using UnityEngine;
 public class Rain : MonoBehaviour
 {
     private float tractionDefault;
+    private float accelerationDefault;
     //Options for reduction
-        // % based aka you have 75% traction while in rain <- with this option we wouldn't need to care about individual players starting points
-        // Flat reduction penalty of number i.e. 3?
+    // % based aka you have 75% traction while in rain <- with this option we wouldn't need to care about individual players starting points
+    // Flat reduction penalty of number i.e. 3?
+
+    //Make rain increase acceleration 
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag =="Player") //In multiplayer we may need a player 1 - X tag for keeping track of individual stats
+        if (other.tag == "Player") //In multiplayer we may need a player 1 - X tag for keeping track of individual stats
         {
-            isColliding = true;
             PlayerStats pStats = other.gameObject.GetComponent<PlayerStats>();
             tractionDefault = pStats.Traction;
             if (tractionDefault > 3)
             {
                 pStats.Traction -= 3;
-            } else
+            }
+            else
             {
                 pStats.Traction = 1;
             }
+
+            accelerationDefault = pStats.Acc;
+            if (accelerationDefault < 8)
+            {
+                pStats.Acc += 3;
+            }
+            else
+            {
+                pStats.Acc = 10;
+            }
+
+            //pStats.Acc *= 1.25f;
             //pStats.Traction *= 0.75f;
         }
     }
@@ -34,6 +49,8 @@ public class Rain : MonoBehaviour
         {
             PlayerStats pStats = other.gameObject.GetComponent<PlayerStats>();
             pStats.Traction = tractionDefault;
+            pStats.Acc = accelerationDefault;
+            //pStats.Acc /= 1.25f;
             //pStats.Traction /= 0.75f;
         }
     }
