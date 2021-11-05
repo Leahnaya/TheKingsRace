84a8d5134c7952086619f1c6484c415ed90beabb
Vincent Wheat <waffleater7@gmail.com>
2021-11-05 13:19:24 -0500

    Script Cleanup
    Cleaned up popup controller

--- Assets/Scripts/PopupController.cs
+++ Assets/Scripts/PopupController.cs
@@ -6,17 +6,6 @@ using UnityEngine.UI;
 public class PopupController : MonoBehaviour
 {
     public GameObject Popup;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if( other.tag == "Player")
