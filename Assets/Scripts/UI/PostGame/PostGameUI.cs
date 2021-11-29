155882e8e686bd36b4d0995372fd07c56b60641c
Julia Butenhoff <cockatoo242@gmail.com>
2021-11-29 14:13:38 -0600

    Added initial objects into PostGame scene

---
+++ Assets/Scripts/UI/PostGame/PostGameUI.cs
@@ -0,0 +1,23 @@
+using MLAPI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostGameUI : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnReturnToLobbyClicked() { 
+        //TODO: Do it here
+    }
+}
