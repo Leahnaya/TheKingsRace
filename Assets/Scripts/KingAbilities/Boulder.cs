acdac8b00ac81fd71f37ed9ad4d034bc9aafd383
Vincent Wheat <60979035+wheatv3015@users.noreply.github.com>
2021-12-10 21:03:36 -0600

    Updates
    -Boulder Prefab
    -Boulder Spawning
    -Plains Layout
    -Painted Terrain

--- Assets/Scripts/KingAbilities/Boulder.cs
+++ Assets/Scripts/KingAbilities/Boulder.cs
@@ -12,11 +12,13 @@ public class Boulder : MonoBehaviour
 
     void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
-            Debug.Log("Crunch");
+            //Debug.Log("Crunch");
             GameObject player = other.gameObject;//Turns the collider into a game object
             Vector3 Dir = boulder.velocity;//Finds the bolder's velocity
-            float Power = boulder.velocity.magnitude * 250;//Finds the power of the boulder by using it's velocity and a scaler
+            float Power = boulder.velocity.magnitude;// * 250;//Finds the power of the boulder by using it's velocity and a scaler
             Dir = Vector3.Normalize(Dir);//Normalizes the vector to be used as a knockback direction
+            //Knock the player a little to the side
+            Dir.z += Random.Range(-0.25f, 0.25f);
             player.GetComponent<dPlayerMovement>().GetHit(Dir, Power); //Ragdolls the player in the direction of the bolder depending on it's speed
         }
     }
