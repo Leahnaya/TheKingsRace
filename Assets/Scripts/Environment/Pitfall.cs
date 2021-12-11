dd9a7b625c22dfdc2720854acfe46d6a6ed000e4
Julia Butenhoff <cockatoo242@gmail.com>
2021-12-10 18:29:16 -0600

    Beginning of KillZone work

--- Assets/Scripts/Environment/Pitfall.cs
+++ Assets/Scripts/Environment/Pitfall.cs
@@ -1,17 +1,39 @@
+using MLAPI;
+using MLAPI.Messaging;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pitfall : MonoBehaviour
+public class Pitfall : NetworkBehaviour
 {
-    public Vector3 RespawnPoint; //Where player will respawn to, set in GUI
+    [SerializeField] private Vector3 RespawnPoint; //Where player will respawn to, set in GUI
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player")) //If runner enters the trigger cancel momentum, disable controls, and move to set respawn point
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.parent.gameObject.tag == "Player" && other.gameObject.transform.parent.gameObject.GetComponent<NetworkObject>().OwnerClientId == NetworkManager.Singleton.LocalClientId) {
+            RespawnPlayerServerRPC();
+        }
+    }
+
+    [ServerRpc]
+    private void RespawnPlayerServerRPC(ServerRpcParams serverRpcParams = default) {
+        RespawnPlayerClientRPC(serverRpcParams.Receive.SenderClientId);
+    }
+
+    [ClientRpc]
+    private void RespawnPlayerClientRPC(ulong playerID) {
+        // Get all players in the scene
+        GameObject[] playableCharacters = GameObject.FindGameObjectsWithTag("Player");
+
+        // Find our player first
+        foreach (GameObject character in playableCharacters)
         {
-            other.transform.position = RespawnPoint;
-            other.GetComponent<PlayerMovement>().CancelMomentum();
+            if (character.GetComponent<NetworkObject>().OwnerClientId == playerID)
+            {
+                character.GetComponentInChildren<PlayerMovement>().CancelMomentum();
+                Debug.Log(character.transform.position);
+                character.transform.position = RespawnPoint;
+                Debug.Log(character.transform.position);
+            }
         }
     }
 }
