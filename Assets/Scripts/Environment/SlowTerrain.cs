a09e513076714a3c2d7fc071e3fa856d0bcccaa8
Vincent Wheat <waffleater7@gmail.com>
2022-02-07 14:34:54 -0600

    Gate prefab updates, net manager updates
    -Game mountain added level
    -added gate model

--- Assets/Scripts/Environment/SlowTerrain.cs
+++ Assets/Scripts/Environment/SlowTerrain.cs
@@ -8,7 +8,6 @@ public class SlowTerrain : MonoBehaviour
     //Also maybe add an isGrounded check?
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Slower!");
         if (other.gameObject.tag == "Player")
         {
             other.GetComponent<PlayerStats>().MaxVel = 10.0f; //if player enters trigger slow them down
@@ -20,7 +19,7 @@ public class SlowTerrain : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            other.GetComponent<PlayerStats>().MaxVel = 25.0f;//if player exits trigger return speed to default
+            other.GetComponent<PlayerStats>().MaxVel = 45.0f;//if player exits trigger return speed to default
         }
     }
 }
