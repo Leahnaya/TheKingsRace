966c85c6462349e3c2672fd63d018d2d73b2dccb
Julia Butenhoff <cockatoo242@gmail.com>
2022-02-23 13:06:24 -0600

    Made gates despawn after being kicked and timer

---
+++ Assets/Scripts/Environment/GateDespawner.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+using MLAPI.Messaging;
+
+public class GateDespawner : NetworkBehaviour {
+
+    private float despawnTime = 10f;
+
+    void Update() { 
+        // Check if the door has been kicked
+        // Object won't be kinematic anymore if kicked
+        if (!this.gameObject.GetComponent<Rigidbody>().isKinematic) { 
+            // Only have the host start the timer
+            if (IsHost) {
+                StartCoroutine(DespawnTimer());
+            }
+        }
+    }
+
+    IEnumerator DespawnTimer() {
+        // Wait for set time
+        yield return new WaitForSecondsRealtime(despawnTime);
+
+        // Despawn the door
+        DespawnGateServerRPC();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void DespawnGateServerRPC() {
+        this.gameObject.GetComponent<NetworkObject>().Despawn(true);
+    }
+
+}
