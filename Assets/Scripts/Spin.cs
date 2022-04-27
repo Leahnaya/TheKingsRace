33a20e03bbd6436aaf3a014016c27d9d70514173
Vincent Wheat <waffleater7@gmail.com>
2022-04-27 17:14:30 -0500

    Fixed Spawning In Scene
    Fixed spawn locations on the new platform
    Increased rotate spin speed
    Moved spawn platform
    Made spin bumpers triggers

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    Vector3 rotation = new Vector3(0.0f, .5f, 0.0f);
+    Vector3 rotation = new Vector3(0.0f, 2.5f, 0.0f);
 
     // Update is called once per frame
     void Update()
